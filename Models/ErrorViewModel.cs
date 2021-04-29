cf041800ad7f4eedfa19f69aee3f667e874f5a7e
Renato Valer <valer9826@gmail.com>
2021-04-29 14:03:25 -0500

    Creación de Proyecto MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LABUSMP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
